f1abd03f92753564e2bbe49831ff7a88af940c0d
Eddcakes <eddadamson@gmail.com>
2019-05-25 01:34:39 +0100

    events draft

--- src/page/Events.js
+++ src/page/Events.js
@@ -1,67 +1,40 @@
 import React, { useEffect } from "react";
+import data from "../data/tournaments.json";
+import venueData from "../data/venues.json";
+import personData from "../data/actors.json";
 
-const heroContainer = {
-  display: "grid",
-  gridTemplateColumns: "repeat(4, 1fr)",
-  gridTemplateRows: "repeat(2, 1fr)",
-  gridTemplateAreas: `'main main one two' 'main main three four'`
-};
-const heroMain = {
-  borderStyle: "solid",
-  height: 600,
-  gridArea: "main"
-};
-const heroOne = {
-  borderStyle: "solid",
-  gridArea: "one"
-};
-const heroTwo = {
-  borderStyle: "solid",
-  gridArea: "two"
-};
-const heroThree = {
-  borderStyle: "solid",
-  gridArea: "three"
-};
-const heroFour = {
-  borderStyle: "solid",
-  gridArea: "four"
-};
-const Events = () => {
+function Events(){
   useEffect(() => {
     document.title = `Events`;
   }, []);
   return (
     <article>
-      <div style={heroContainer} className="hero-container">
-        <section className="hero-main" style={heroMain}>
-          <header>
-            <h2>Title of content</h2>
-          </header>
-        </section>
-        <section className="hero-sm" style={heroOne}>
-          <header>
-            <h3>Title of content</h3>
-          </header>
-        </section>
-        <section className="hero-sm" style={heroTwo}>
-          <header>
-            <h3>Title of content</h3>
-          </header>
-        </section>
-        <section className="hero-sm" style={heroThree}>
-          <header>
-            <h3>Title of content</h3>
-          </header>
-        </section>
-        <section className="hero-sm" style={heroFour}>
-          <header>
-            <h3>Title of content</h3>
-          </header>
-        </section>
-      </div>
+      {data.map( tournament => (
+        <Event key={tournament._id} {...tournament}/>
+      ))
+      }
     </article>
   );
 };
+function Event({event, date, venue_id, winner_id, runnerUp_id, ...props}){
+  return(
+    <article>
+      <h1>{event}</h1>
+      <div>{date}</div>
+      <div>{lookupVenue(venue_id)}</div>
+      <div>Winnder: {lookupPerson(winner_id)}</div>
+      <div>Runner up: {lookupPerson(runnerUp_id)}</div>
+    </article>
+  )
+}
+
+function lookupPerson(id){
+  let actor = personData.filter( person => person.id === id )
+  return actor[0].name
+}
+function lookupVenue(id){
+  let place = venueData.filter( venue => venue.ID === id )
+  return place[0].name
+}
 
 export default Events;
